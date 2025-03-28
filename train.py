7173e40bc39bc2e7efaba9699924c91480b0dfef
wjm55 <william.mattingly@yale.edu>
2025-03-28 15:01:34 -0300

    Refactor training script to support multiple YOLO model variants and optimize training parameters

--- train.py
+++ train.py
@@ -1,6 +1,23 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
+models = [
+    # "yolo11n.pt",
+    "yolo11s.pt",
+    "yolo11m.pt",
+    "yolo11l.pt",
+    "yolo11x.pt"
+    ]
 
-model.train(data="data.yaml", epochs=100)
+for model_name in models:
+    model = YOLO(model_name)  # load a pretrained model
 
+        # Train the model with memory-saving parameters
+    results = model.train(
+        data="data.yaml",
+        epochs=100,
+        imgsz=640,
+        batch=8,  # Reduce batch size (default is 16)
+        # cache=False,  # Disable caching
+        name=f"ms_{model_name.replace('.pt', '')}",
+        workers=2  # Reduce number of workers
+    )
